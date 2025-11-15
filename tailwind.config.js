7a8917aa203e705ce0394318b8ad6a11be0584ff
mestrereginaldo-site <reginaldo.oliveira@catolicasc.edu.br>
2025-11-15 11:47:51 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,18 @@
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1e40af',
+        secondary: '#1e3a8a',
+        accent: '#f59e0b',
+      }
+    },
+  },
+  plugins: [],
+}
